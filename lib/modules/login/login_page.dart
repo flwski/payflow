19b0713c1b5dcb9ef13e7a761bc1d0934848fe72
1172 <fernandolevandoskip@gmail.com>
2021-07-09 11:06:11 -0300

    Criando tela inicial e Google Auth

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +14,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -46,18 +50,18 @@ class _LoginPageState extends State<LoginPage> {
                 children: [
                   Image.asset(AppImages.logomini),
                   Padding(
-                    padding: const EdgeInsets.only(top:30,left: 70, right: 70),
-                    child: Text(                      
-                      "Organize seus boletos em um só lugar.", 
-                      textAlign: TextAlign.center,                      
-                      style: TextStyles.titleHome
-                      ),
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text("Organize seus boletos em um só lugar.",
+                        textAlign: TextAlign.center,
+                        style: TextStyles.titleHome),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(left: 40, right: 40, top:40),
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
                     child: SocialLoginButton(
-                      onTap: (){
-                        print("oi");
+                      onTap: () {
+                        controller.googleSignIn(context);
                       },
                     ),
                   )
